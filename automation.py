6b6cd95744b7942f10b1ce916d2f54c9080dcd25
Michael Wheeler <michael@michael-wheeler.org>
2013-02-09 19:02:41 +1000

    Change analog reading and a few other vars

--- automation.py
+++ automation.py
@@ -3,11 +3,9 @@ import time
 import cherrypy
 import threading
 import sys
-from cherrypy.lib.static import serve_file 
+from cherrypy.lib.static import serve_file
 
 STATICPATH = '/root/Home-Automation-Python/static'
-THRESHOLD = 1
-CACHETIME = 0.5 #amount of time in seconds to cache analogs
 serialMutex = threading.Lock()
 config = {'/': {'tools.staticdir.on': True, 'tools.staticdir.dir':STATICPATH}}
 cherrypy.config.update({'server.socket_host': '0.0.0.0','server.socket_port': 8080})
@@ -20,18 +18,12 @@ class powerSwitch:
         self.description = description
         self.analogPin = analogPin
         self.state = False
+        self.cachedState = -1  #set the cached state to -1, unlike state, cachedstate is the analog reading
         board.output([pinNumber])
         board.analogWrite(self.pinNumber,0)
         board.setLow(pinNumber)
-        self.isOnCache = False
         if (analogPin == -1):
             self.analogPin = pinNumber-22
-            
-        if (int(board.analogRead(self.analogPin)) < THRESHOLD):
-            self.isOnCache = False
-        else:
-            self.isOnCache = True
-        self.isOnCacheTimeout = time.time() + CACHETIME
         serialMutex.release()
     def toggle(self):
         serialMutex.acquire()
@@ -45,33 +37,28 @@ class powerSwitch:
             #board.analogWrite(self.pinNumber,0)
         serialMutex.release()
     def off(self):
-        if (self.isOn(checkCache=False)):
+        if (self.isOn()):
+            self.cachedState = -1
             self.toggle()
     def on(self):
-        if (not self.isOn(checkCache=False)):
+        if (not self.isOn()):
+            self.cachedState = -1
             self.toggle()
-    def isOn(self, checkCache=True):
-        if (time.time() < self.isOnCacheTimeout) and checkCache: #use cache if it's fresh and we are allowed
-            return self.isOnCache
-        elif(serialMutex.acquire(False)): #check if we can refresh the cache without blocking
-            if (int(board.analogRead(self.analogPin)) < THRESHOLD):
-                 self.isOnCache = False
-            else:
-                 self.isOnCache = True
-            serialMutex.release()
-            self.isOnCacheTimeout = time.time() + CACHETIME
-            return self.isOnCache
-        elif(checkCache==False):         #if we can't check without blocking and we need fresh data, block
+    def isOn(self):
             serialMutex.acquire()
-            if (int(board.analogRead(self.analogPin)) < THRESHOLD):
-                 self.isOnCache = False
+            if (int(board.analogRead(self.analogPin))):
+                 serialMutex.release()
+                 return True
             else:
-                 self.isOnCache = True
-            serialMutex.release()
-            self.isOnCacheTimeout = time.time() + CACHETIME
-            return self.isOnCache            
-        else:                              #else just send the cached data.
-            return self.isOnCache
+                 serialMutex.release()
+                 return False
+    def cachedRead(self):
+        if self.cachedState != -1:
+             return self.cachedState
+        else:
+             self.cachedState = self.isOn()
+    def updateCache(self):
+        self.cachedState = self.isOn()
 
 
 #doors that have an unlock switch
@@ -119,18 +106,18 @@ class door:
 
 class webMappings(object):
     def index(self):
-        return serve_file(STATICPATH + "/index.html") 
-    def on(self, switchId):
+        return serve_file(STATICPATH + "/index.html")
+    def on(self, switchId, NOCACHE=False):
         switches[int(switchId)].on()
         return "On"
-    def off(self, switchId):
+    def off(self, switchId, NOCACHE=False):
         switches[int(switchId)].off()
         return "Off"
-    def unlock(self, timer=20):
+    def unlock(self, timer=20, NOCACHE=False):
         t = threading.Thread(target=frontDoor.doorUnlock, args=(timer,))
         t.start()
         return "Unlocked"
-    def status(self):
+    def status(self, NOCACHE=False):
         y = 0
         page = "{\"description\": {"
         for switch in switches:
@@ -143,7 +130,7 @@ class webMappings(object):
         for switch in switches:
             if y != 0:
                 page = page + ",\n"
-            if switch.isOn():
+            if switch.cachedRead():
                 page = page + "\"" + str(y) + "\"" + " : 1"
             else:
                 page = page + "\"" + str(y) + "\"" + " : 0"
@@ -159,37 +146,59 @@ class webMappings(object):
 class webServer(threading.Thread):
     def run(self):
         cherrypy.quickstart(webMappings(),'/',config=config)
-        
+
 class checkDoor(threading.Thread):
     def run(self):
         frontDoor.checkSwitch(loop=True)
 
-
+class updateSwitchCache(threading.Thread):
+    def run(self):
+        self.breakLoop = 0
+        while self.breakLoop == 0:
+           time.sleep(0.3)
+           print "Updating switch cache"
+           for switch in switches:
+               switch.updateCache()
 
 def exit():
     cherrypy.engine.exit()
     frontDoor.breakLoop = 1
+    t0.breakLoop = 1
     sys.exit("Killed by user")
-	
 
-board = Arduino('/dev/ttyACM0')
+
+board = Arduino('/dev/ttyUSB0')
+#switches = [
+#powerSwitch(22,"Front Light"),
+#powerSwitch(23,"Kitchen Light"),
+#powerSwitch(24,"Dinning Room Light"),
+#powerSwitch(25,"Lounge Room 1 Light"),
+#powerSwitch(26,"Lounge Room 2 Light"),
+#powerSwitch(27,"Bedroom 1 Light"),
+#powerSwitch(28,"Bedroom 1 Fan"),
+#powerSwitch(29,"Bedroom 2 Light"),
+#powerSwitch(30,"Bedroom 2 Fan"),
+#powerSwitch(31,"Bedroom 3 Light"),
+#powerSwitch(32,"Bedroom 3 Fan"),
+#powerSwitch(33,"Hallway"),
+#powerSwitch(34,"Outside 1 Light"),
+#powerSwitch(35,"Outside 2 Light"),
+#]
 
 switches = [
 powerSwitch(22,"Front Light"),
 powerSwitch(23,"Kitchen Light"),
 powerSwitch(24,"Dinning Room Light"),
-powerSwitch(25,"Lounge Room 1 Light"),
-powerSwitch(26,"Lounge Room 2 Light"),
-powerSwitch(27,"Bedroom 1 Light"),
-powerSwitch(28,"Bedroom 1 Fan"),
+powerSwitch(26,"Lounge Room 1 Light"),
+powerSwitch(25,"Lounge Room 2 Light"),
+powerSwitch(27,"Lounge Room Fan"),
+powerSwitch(28,"Bedroom 1 Light"),
+powerSwitch(32,"Bedroom 1 Fan"),
 powerSwitch(29,"Bedroom 2 Light"),
-powerSwitch(30,"Bedroom 2 Fan"),
-powerSwitch(31,"Bedroom 3 Light"),
-powerSwitch(32,"Bedroom 3 Fan"),
-powerSwitch(33,"Hallway"),
-powerSwitch(34,"Outside 1 Light"),
-powerSwitch(35,"Outside 2 Light"),
+powerSwitch(31,"Bedroom 2 Fan"),
+powerSwitch(30,"Bedroom 3 Light")
 ]
+
 
 frontDoor = door(43,45)
 
@@ -197,8 +206,11 @@ t = webServer()
 t.start()
 t = checkDoor()
 t.start()
+t0 = updateSwitchCache()
+t0.start()
 
 while(1):
-	command = raw_input("> ")
-	if command == "exit":
-		exit()
+        command = raw_input("> ")
+        if command == "exit":
+                exit()
+
--- prototype/prototype.ino
+++ prototype/prototype.ino
@@ -1,6 +1,30 @@
+#include <Average.h>
+// record the last 10 readings of the 16 analog inputs
+int analogvalues[16][10] = {0};
+// what number was the last read analoginput pin
+int lastread = -1;
+// The current index
+int index = 0;
+
+void checkAnalog(){
+  lastread++;
+  if (lastread == 16){
+   lastread = 0;
+   index++; 
+  }
+  if (index == 10){
+   index = 0;
+  }
+  analogvalues[lastread][index] = analogRead(lastread);
+  // increment lastread +1
+  // if lastread now equals 16, reset to 0 and increament index
+  // if index now equals 10, reset to 0
+  // read analog pin and place in analogvalues[lastread][index]
+}
+
 void setup() {
     Serial.begin(115200);
-    Serial.setTimeout(5);
+    Serial.setTimeout(20);
 
     int cmd = readData();
     for (int i = 0; i < cmd; i++) {
@@ -24,7 +48,7 @@ void loop() {
             analogWrite(readData(), readData()); break;
         case 4 :
             //read analog value
-            Serial.println(analogRead(readData()));
+            Serial.println(maximum(analogvalues[readData()],10));
     }
 }
 
@@ -34,5 +58,6 @@ char readData() {
         if(Serial.available() > 0) {
             return Serial.parseInt();
         }
+        checkAnalog();
     }
 }
